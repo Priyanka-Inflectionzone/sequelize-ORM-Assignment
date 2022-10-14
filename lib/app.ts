fbac00789ab2c088f5f83f63418b51e86d3fb750
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2022-10-14 10:20:03 +0530

    restarting project

--- lib/app.ts
+++ lib/app.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as errorhandler from 'strong-error-handler';
-import { podcasts } from './routes/podcasts';
-import { artists } from './routes/artists';
-import { users } from './routes/users';
+// import { podcasts } from './routes/podcasts';
+// import { artists } from './routes/artists';
+// import { users } from './routes/users';
 
 export const app = express();
 
@@ -23,9 +23,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use('/podcasts', podcasts);
-app.use('/artists', artists);
-app.use('/users', users);
+// app.use('/podcasts', podcasts);
+// app.use('/artists', artists);
+// app.use('/users', users);
 
 app.use(
 	errorhandler({
